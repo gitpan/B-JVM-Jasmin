ec204c62b3166f4dc5264d9ecc00574c592bbe47
Bradley M. Kuhn <bkuhn@ebb.org>
1999-12-05 08:34:32 +0000

    Import of BKUHN/B-JVM-Jasmin-0.02 from CPAN.
    gitpan-cpan-distribution: B-JVM-Jasmin
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BKUHN/B-JVM-Jasmin-0.02.tar.gz
    gitpan-cpan-author:       BKUHN
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -1,23 +1,162 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl test.pl'
+#!perl -I ../blib/lib
 
-######################### We start with some black magic to print on failure.
+# Last change: Fri May 28 03:16:57 BST 1999
 
-# Change 1..1 below to 1..last_test_to_print .
-# (It may become useful if the test is moved to ./t subdirectory.)
+# This is written in a peculiar style, since we're trying to avoid
+# most of the constructs we'll be testing for.
 
-BEGIN { $| = 1; print "1..1\n"; }
-END {print "not ok 1\n" unless $loaded;}
-use B::JVM::Jasmin;
-$loaded = 1;
-print "ok 1\n";
+$| = 1;
 
-######################### End of black magic.
+if ($#ARGV >= 0 && $ARGV[0] eq '-v') {
+    $verbose = 1;
+    shift;
+}
 
-# Insert your test code below (better if it prints "ok 13"
-# (correspondingly "not ok 13") depending on the success of chunk 13
-# of the test code):
-my $INC = '-I' . join(' -I', @INC);
-`perl -MO=JVM::Jasmin,DemoClass $INC example.pl`;
-print `jasmin DemoClass.asm`;
-print `java DemoClass`;
+chdir 'tests';
+
+die "You need to run \"make test\" first to set things up.\n"
+  unless -e '../perljvm';
+
+# check leakage for embedders
+$ENV{PERL_DESTRUCT_LEVEL} = 2 unless exists $ENV{PERL_DESTRUCT_LEVEL};
+
+$ENV{EMXSHELL} = 'sh';        # For OS/2
+
+if ($#ARGV == -1) {
+    @ARGV = split(/[ \n]/, `echo not-from-perl/*.t base/if.t`);
+}
+
+_testprogs('perljvm', @ARGV);
+
+sub _testprogs {
+    $type = shift @_;
+    @tests = @_;
+
+
+    print <<'EOT' if ($type eq 'compile');
+--------------------------------------------------------------------------------
+TESTING COMPILER
+--------------------------------------------------------------------------------
+EOT
+
+    $ENV{PERLCC_TIMEOUT} = 120 
+          if ($type eq 'compile' && !$ENV{PERLCC_TIMEOUT});
+
+    $bad = 0;
+    $good = 0;
+    $total = @tests;
+    $files  = 0;
+    $totmax = 0;
+    $maxlen = 0;
+    foreach (@tests) {
+	$len = length;
+	$maxlen = $len if $len > $maxlen;
+    }
+    # +3 : we want three dots between the test name and the "ok"
+    # -2 : the .t suffix
+    $dotdotdot = $maxlen + 3 - 2;
+    while ($test = shift @tests) {
+
+	if ($test =~ /^$/) {
+	    next;
+	}
+	$te = $test;
+	chop($te);
+	print "$te" . '.' x ($dotdotdot - length($te));
+
+	open(SCRIPT,"<$test") or die "Can't run $test.\n";
+	$_ = <SCRIPT>;
+	close(SCRIPT);
+	if (/#!.*perl(.*)$/) {
+	    $switch = $1;
+	    if ($^O eq 'VMS') {
+		# Must protect uppercase switches with "" on command line
+		$switch =~ s/-([A-Z]\S*)/"-$1"/g;
+	    }
+	}
+	else {
+	    $switch = '';
+
+	}
+	if ($type eq 'perljvm') {
+            $INC = ' -I' . join(' -I', @INC);
+	    open(RESULTS, "$^X $INC ../perljvm --run $test |") or print "can't run.\n";
+	}
+
+	$ok = 0;
+	$next = 0;
+	while (<RESULTS>) {
+	    if ($verbose) {
+		print $_;
+	    }
+	    unless (/^(#|Generated:)/) {
+		if (/^1\.\.([0-9]+)/) {
+		    $max = $1;
+		    $totmax += $max;
+		    $files += 1;
+		    $next = 1;
+		    $ok = 1;
+		}
+		else {
+		    $next = $1, $ok = 0, last if /^not ok ([0-9]*)/;
+		    if (/^ok (\d+)(\s*#.*)?$/ && $1 == $next) {
+			$next = $next + 1;
+		    }
+		    else {
+			$ok = 0;
+		    }
+		}
+	    }
+	}
+	close RESULTS;
+	$next = $next - 1;
+	if ($ok && $next == $max) {
+	    if ($max) {
+		print "ok\n";
+		$good = $good + 1;
+	    }
+	    else {
+		print "skipping test on this platform\n";
+		$files -= 1;
+	    }
+	}
+	else {
+	    $next += 1;
+	    print "FAILED at test $next\n";
+	    $bad = $bad + 1;
+	    $_ = $test;
+	    if (/^base/) {
+		die "Failed a basic test--cannot continue.\n";
+	    }
+	}
+    }
+
+    if ($bad == 0) {
+	if ($ok) {
+	    print "All tests successful.\n";
+	}
+	else {
+	    die "FAILED--no tests were run for some reason.\n";
+	}
+    }
+    else {
+	$pct = sprintf("%.2f", ($files - $bad) / $files * 100);
+	if ($bad == 1) {
+	    warn "Failed 1 test script out of $files, $pct% okay.\n";
+	}
+	else {
+	    warn "Failed $bad test scripts out of $files, $pct% okay.\n";
+	}
+	warn <<'SHRDLU';
+   ### Since not all tests were successful, you may want to run some
+   ### of them individually and examine any diagnostic messages they
+   ### produce.
+   ### You can run them using:
+   ###                $^X $INC ../perljvm --run
+SHRDLU
+    }
+    ($user,$sys,$cuser,$csys) = times;
+    print sprintf("u=%g  s=%g  cu=%g  cs=%g  scripts=%d  tests=%d\n",
+	$user,$sys,$cuser,$csys,$files,$totmax);
+}
+exit ($bad != 0);
